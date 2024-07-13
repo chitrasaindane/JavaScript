b91026b92a903d44a544e9b3dbfe31856ca516de
chitrasaindane <163283978+chitrasaindane@users.noreply.github.com>
2024-07-13 09:54:31 +0000

    comparison note

---
+++ javascript/comparison.js
@@ -0,0 +1,20 @@
+console.log("Number and string conversion")
+console.log("2" >1)
+
+
+console.log("null and zero conversion")
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) // true
+
+console.log("undefined and zero conversion")
+//All are false
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+
+
+// === not allow to compare different datatype
+console.log("=== conversion")
+console.log("5" === 5)
