108e20b7926fd46485a8f7980d39a80f5facdfa3
Chitra Saindane <163283978+chitrasaindane@users.noreply.github.com>
2024-07-15 18:04:20 +0000

    function

---
+++ javascript/function.js
@@ -0,0 +1,13 @@
+
+function MyName(){
+    console.log("CHITRA")
+}
+MyName()
+
+function addition(a,b){
+    console.log(a+b)
+}
+addition(2,null)
+addition(3,"a")
+addition(3,5)
+addition(7,undefined)
