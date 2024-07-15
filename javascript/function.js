f59afe9b380c7695b57bcb70e6df56d018e8e873
Chitra Saindane <163283978+chitrasaindane@users.noreply.github.com>
2024-07-15 18:24:36 +0000

    function

--- javascript/function.js
+++ javascript/function.js
@@ -11,3 +11,35 @@ addition(2,null)
 addition(3,"a")
 addition(3,5)
 addition(7,undefined)
+
+function login(username){
+    return `${username} just logged in `
+}
+console.log(login("chitra"))
+console.log(login())
+console.log(login(" "))
+
+function calculateCartPrice(val1 , ...num1){
+ return num1
+}
+console.log(calculateCartPrice(200,890,100))
+
+// object in function
+
+const user ={
+    name:"diya",
+    price: 200
+}
+function handleobject(anyobj){
+    console.log(`username is ${anyobj.name} and price is ${anyobj.price}`)
+}
+handleobject(user)
+
+// array in function
+
+const array=[100,200,300]
+
+function arrayvalue(array){
+    return array[2]
+}
+console.log(arrayvalue(array))
