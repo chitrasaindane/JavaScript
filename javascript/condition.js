9d6d2fda6529fd47399081a7553840025067dd7d
Chitra Saindane <163283978+chitrasaindane@users.noreply.github.com>
2024-07-17 18:21:09 +0000

    condition

---
+++ javascript/condition.js
@@ -0,0 +1,15 @@
+let month =1
+switch(month){
+    case 1:
+        console.log("january")
+        break
+    case 2:
+        console.log("feb")
+        break
+    case 3:
+        console.log("mar")
+        break
+    default:
+            console.log("enter valid no.")
+        break
+}
